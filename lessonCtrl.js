c97fab01700ce8065f9357eec8e0922342bef704
ToasterTom <tate.thomas5595@gmail.com>
2016-10-11 20:17:55 -0600

    Final Step Complete

--- lessonCtrl.js
+++ lessonCtrl.js
@@ -1,3 +1,10 @@
 angular.module('directivePractice').controller('lessonCtrl', function ($scope, lessonService) {
 
+$scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+
+
+$scope.announceDay = function (lesson, day) {
+  alert(lesson + ' is active on ' + day + '.');
+}
+
 })
--- lessonHider.js
+++ lessonHider.js
@@ -1,5 +1,30 @@
 angular.module('directivePractice')
 
 .directive('lessonHider', function () {
+  return {
+    templateUrl: 'lessonHider.html',
+    restrict: 'E',
+    controller: function ($scope, lessonService) {
+      $scope.getSchedule = lessonService.getSchedule();
+    },
+    scope: {
+      lesson: '=',
+      dayAlert: '&',
 
+    },
+    link: function (scope, element, attributes) {
+      scope.getSchedule.then(function (response) {
+        scope.schedule = response.data;
+        console.log(response);
+
+      scope.schedule.forEach(function (day) {
+        if (day.lesson === scope.lesson) {
+          element.css('text-decoration','line-through');
+          scope.lessonDay = day.weekday;
+          return;
+        }
+      })
+      });
+    }
+  }
 })
